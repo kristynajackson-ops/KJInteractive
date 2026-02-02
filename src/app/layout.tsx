abfc305b9b716c29464a0ceb7c5ca19f48b19e0a
Krist <krist@example.com>
2026-02-02 18:58:12 +1100

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat, Roboto, Inter } from 'next/font/google'
 import './globals.css'
 import Nav from '../components/Nav'
 import { LinkedInFooterLogo } from '../components/LinkedInFooterLogo'
+import { Analytics } from '@vercel/analytics/next'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Nav />
         {children}
+        <Analytics />
         <footer className="bg-gradient-to-r from-[#5f5e5c] to-[#6a6665] text-white py-4 mt-0 text-sm">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
             <div className="flex-1 flex justify-end">
